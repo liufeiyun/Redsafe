febf4135837785ee24ac9a25ac0fc5d2e5bc7b3f
fyliu <1220429263@qq.com>
2018-04-21 12:50:32 +0800

    解决Tortoisegit其他都是默认安装配置，但pull的时候与git冲突的问题

--- src/com/lfy/app/Hello.java
+++ src/com/lfy/app/Hello.java
@@ -1,7 +1,8 @@
-public class Hello
+﻿public class Hello
 {
 	public static void main(String[] args){
 	   System.out.println("Hello World");
 	   System.out.println("git operation show");
+	   System.out.println("Correct Tortoisegit TortoiseGit and Git conflict");
 	}
 }
